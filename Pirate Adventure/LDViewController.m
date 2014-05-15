f59da5b447e08939d3310aab1167438547e3683c
“lisadean” <lisa@lisadean.net>
2014-05-14 21:06:28 -0400

    Initial Commit

---
+++ Pirate Adventure/LDViewController.m
@@ -0,0 +1,29 @@
+//
+//  LDViewController.m
+//  Pirate Adventure
+//
+//  Created by Lisa Dean on 5/14/14.
+//
+//
+
+#import "LDViewController.h"
+
+@interface LDViewController ()
+
+@end
+
+@implementation LDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
