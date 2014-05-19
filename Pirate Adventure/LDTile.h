8a5317964120e51ccef22f51b558f26c09087ab6
“lisadean” <lisa@lisadean.net>
2014-05-19 00:19:12 -0400

    Temporarily update health, damage, weapon and armor labels on load — needs to be moved to loadNewTile method. Create character on load. Changed directional buttons back to default formatting and added View object for them to live in. Added loadNewtile method: sets background image, story label, action button title, hides or unhides directional buttons. Added methods for directional button presses: verifies tile validity and calls loadNewTile method. Cleaned up tile set creation based on what Ty showed me. Fewer variables. Started creating second createTile method with special action — need to think this through. Added character, weapon and armor creation method to factory.

--- Pirate Adventure/LDTile.h
+++ Pirate Adventure/LDTile.h
@@ -7,10 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LDWeapon.h"
+#import "LDArmor.h"
 
 @interface LDTile : NSObject
 
 @property (strong, nonatomic) NSString *story;
 @property (strong, nonatomic) UIImage *backgroundImage;
+@property (strong, nonatomic) NSString *actionButtonTitle;
+@property (strong, nonatomic) LDWeapon *weapon;
+@property (strong, nonatomic) LDArmor *armor;
+@property (nonatomic) int healthEffect;
 
 @end
