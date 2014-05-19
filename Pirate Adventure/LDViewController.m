03061f6ff5314198918bce441ffc3937a29f8e69
Lisa Dean <lisa@lisadean.net>
2014-05-19 13:49:16 -0400

    Update LDViewController.m
    Testing editing from github.com web interface

--- Pirate Adventure/LDViewController.m
+++ Pirate Adventure/LDViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
+    // test edit from github.com
     // Create tileset and character
     self.tileSet = [LDFactory createTileSet];
     self.character = [LDFactory createCharacter];
